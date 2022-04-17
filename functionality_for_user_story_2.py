bc61acadc2fa23676ecea9592fdf3d4c41e3286c
razrobill <tb1126@york.ac.uk>
2022-04-17 11:12:26 +0100

    ~

---
+++ functionality_for_user_story_2.py
@@ -0,0 +1,23 @@
+#user story 2
+#see booking informations from form.
+#needs database
+
+
+
+
+
+
+
+
+
+#user story 3
+while j < 20:
+    user = input("would you like to go to class? y/n")
+    if user = "y":
+        j = j+1
+
+except j > 20:
+    print("class is full")
+
+
+
